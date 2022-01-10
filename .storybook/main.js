e8a1b67cab7d05bd30e602e71e7e7eafc362e927
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2022-01-10 12:06:34 +0100

    It does not work

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../src/components/usermessages/errorusermessage.stories.js'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+};
---
+++ src/components/usermessages/errorusermessage stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ErrorUserMessage from 'components/usermessages/errorusermessage';
+
+export default { title: 'ErrorUserMessage', component: ErrorUserMessage };
+
+export const Error = () => <ErrorUserMessage></ErrorUserMessage>;
