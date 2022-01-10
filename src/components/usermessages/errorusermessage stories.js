299c4fb2dde316223dabbcfb99be5df6557df71b
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2022-01-10 13:25:07 +0100

    Added storybook

--- src/components/usermessages/errorusermessage stories.js
+++ src/components/usermessages/errorusermessage stories.js
@@ -4,4 +4,21 @@ import ErrorUserMessage from 'components/usermessages/errorusermessage';
 const l = { title: 'ErrorUserMessage', component: ErrorUserMessage };
 export default l;
 
-export const Error = () => <ErrorUserMessage></ErrorUserMessage>;
+const Story = args => <ErrorUserMessage {...args} />;
+
+export const ErrorMessage = Story.bind({});
+ErrorMessage.args = {
+  title: 'This is the error title'
+};
+
+export const Title = Story.bind({});
+Title.args = {
+  ...ErrorMessage.args,
+  message: 'This is the message'
+};
+
+export const StackErrorMessage = Story.bind({});
+StackErrorMessage.args = {
+  ...Title.args,
+  stack: 'This is the stack'
+};
