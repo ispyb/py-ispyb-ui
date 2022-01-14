e69929c65f99eb801253cba9368947bbcb669695
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2022-01-14 15:14:40 +0100

    Noved storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,15 @@ module.exports = {
   env: {
     node: true,
     commonjs: true,
-    browser: true,
+    browser: true
   },
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:storybook/recommended"],
   rules: {
     'no-alert': 'off',
     'no-console': 'off',
-    'no-unused-vars': 'warn',
-  },
+    'no-unused-vars': 'warn'
+  }
 };
