d165daee3c8e2b0fd479c659cb353cf8e42637bb
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2022-01-28 08:16:43 +0100

    ADding dates

--- src/pages/session/sessiontablemenu.tsx
+++ src/pages/session/sessiontablemenu.tsx
@@ -1,12 +1,12 @@
-import ActionToggleButton from 'components/buttons/actiontogglebutton';
-import { ButtonGroup, Form } from 'react-bootstrap';
+import React from 'react';
 import Menu from 'components/menu/menu';
+import { DateRangePicker } from 'react-dates';
+import { ButtonGroup, Form } from 'react-bootstrap';
+import ActionToggleButton from 'components/buttons/actiontogglebutton';
 import { ActionToggleButtonType } from 'components/buttons/actiontogglebutton';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css';
-import React from 'react';
-
 interface SessionTableMenuType {
   items: Array<ActionToggleButtonType>;
   SearchMenu?: React.ElementType;
@@ -21,6 +21,21 @@ export default function SessionTableMenu({ items, SearchMenu }: SessionTableMenu
         ))}
       </ButtonGroup>
 
+      <DateRangePicker
+      //showClearDates
+      //startDate={this.state.startDate}
+      //startDateId="your_unique_start_date_id"
+      //endDate={this.state.endDate}
+      //endDateId="your_unique_end_date_id"
+      // onDatesChange={this.handleDatesChange}
+      // focusedInput={this.state.focusedInput}
+      //onFocusChange={(focusedInput) => this.setState({ focusedInput })}
+      //daySize={30}
+      //displayFormat="DD/MM/YYYY"
+      //small
+      //isOutsideRange={() => false}
+      //showDefaultInputIcon
+      />
       <Form className="d-flex">{SearchMenu}</Form>
     </Menu>
   );
