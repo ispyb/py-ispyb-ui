035879a9256359cc8d74ddf372a61ca22274ae2c
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2022-02-08 14:19:26 +0100

    Added cosmo style

--- src/config/ui.js
+++ src/config/ui.js
@@ -1,5 +1,5 @@
 const UI = {
-  theme: 'flatly', //(examples: darkly, slate, cosmo, spacelab, and superhero, minty, flatly. See https://bootswatch.com/ for current theme names.
+  theme: 'cosmo', //(examples: darkly, slate, cosmo, spacelab, and superhero, minty, flatly. See https://bootswatch.com/ for current theme names.
   sessionsPage: {
     areMXColumnsVisible: true,
     areSAXSColumnsVisible: false,
