ee49e024d7b5ad6dd880496e906b21d47a01c37b
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2022-02-08 15:57:23 +0100

    Adde thresholds

--- src/pages/em/movie/moviespage.tsx
+++ src/pages/em/movie/moviespage.tsx
@@ -1,34 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useMoviesByDataCollectionId } from 'hooks/ispyb';
 import { Card, ButtonGroup, Badge, ToggleButton } from 'react-bootstrap';
 import MoviePanel from 'pages/em/movie/moviepanel';
 import Menu from 'components/menu/menu';
-import { createImageNumberAndFileName } from 'pages/em/movie/helper';
+import { createImageNumberAndFileName, isMotionThreshold, isResolutionLimitThreshold } from 'pages/em/movie/helper';
 import { Movie } from 'pages/em/model';
 
 export default function MoviesPage() {
   const { dataCollectionId = '', proposalName = '' } = useParams<{ dataCollectionId: string; proposalName: string }>();
 
+  const [isMotion, setisMotion] = useState(false);
+  const [isResolution, setisResolution] = useState(false);
+
   const { data: movies, isError: sessionError } = useMoviesByDataCollectionId({ proposalName, dataCollectionId: parseFloat(dataCollectionId) });
   if (sessionError) throw Error(sessionError);
 
   /** Recalculation imageNumber and File name */
-  const parsedMovies = createImageNumberAndFileName(movies);
+  let parsedMovies = createImageNumberAndFileName(movies);
 
-  //const moviesReachingResolutionLimitThreshold = movies.filter((movie: Movie) => isResolutionLimitThreshold(movie));
-  //const moviesReachingMoviewThreshold = movies.filter((movie: Movie) => isMotionThreshold(movie));
+  const moviesReachingResolutionLimitThreshold = parsedMovies.filter((movie: Movie) => isResolutionLimitThreshold(movie));
+  const moviesReachingMoviewThreshold = parsedMovies.filter((movie: Movie) => isMotionThreshold(movie));
 
-  //const e = <Badge bg="danger">{moviesReachingMoviewThreshold.length}</Badge>;
+  if (isMotion) {
+    parsedMovies = moviesReachingMoviewThreshold;
+  }
+
+  if (isResolution) {
+    parsedMovies = moviesReachingResolutionLimitThreshold;
+  }
+
   return (
     <div style={{ marginBottom: '100px' }}>
       <Menu>
         <ButtonGroup className="mb-2">
-          <ToggleButton id="tbg-btn-1" value={1} style={{ margin: 1 }} size="sm" type="checkbox" onClick={() => alert('test')}>
-            Motion Threshold <Badge bg="warning">asdsad</Badge>
+          <ToggleButton
+            variant={isMotion ? 'outline-primary' : 'light'}
+            checked={isMotion}
+            id="tbg-btn-1"
+            value={1}
+            style={{ margin: 1 }}
+            size="sm"
+            type="checkbox"
+            onClick={() => setisMotion(!isMotion)}
+          >
+            Motion Threshold <Badge bg="danger">{moviesReachingMoviewThreshold.length}</Badge>
           </ToggleButton>
-          <ToggleButton id="tbg-btn-2" value={2} style={{ margin: 1 }} size="sm" type="checkbox" onClick={() => alert('test')}>
-            Resolution Threshold
+          <ToggleButton
+            variant={isResolution ? 'outline-primary' : 'light'}
+            checked={isResolution}
+            id="tbg-btn-2"
+            value={2}
+            style={{ margin: 1 }}
+            size="sm"
+            type="checkbox"
+            onClick={() => setisResolution(!isResolution)}
+          >
+            Resolution Threshold <Badge bg="danger">{moviesReachingResolutionLimitThreshold.length}</Badge>
           </ToggleButton>
         </ButtonGroup>
       </Menu>
