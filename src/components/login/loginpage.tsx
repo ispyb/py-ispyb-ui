f325702f5c4cbe8d307b0cd9aa4fa871194f0575
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2022-02-09 20:28:10 +0100

    Added carousel movies

--- src/components/login/loginpage.tsx
+++ src/components/login/loginpage.tsx
@@ -44,6 +44,16 @@ function LoginPage() {
               </Carousel.Item>
 
               <Carousel.Item style={{ textAlign: 'center', margin: '-50' }}>
+                <img className="w-50" src="/images/carousel/movies.jpeg" alt="Second slide" />
+                <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+                <Carousel.Caption style={{ textAlign: 'center', margin: '-50' }}>
+                  <br />
+                  <h3>EM Movies statistics</h3>
+                  <p>Motion correction drift and CFT</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+
+              <Carousel.Item style={{ textAlign: 'center', margin: '-50' }}>
                 <img
                   className="w-50"
                   src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.g2crowd.com%2Fuploads%2Fproduct%2Fimage%2Fsocial_landscape%2Fsocial_landscape_048daf32d4748a4dcd8a38617af4ff85%2Fkeycloak.png&f=1&nofb=1"
