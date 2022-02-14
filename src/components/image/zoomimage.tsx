02f9b9ccb97ee73a69cd5132474747e8cd81517a
Mael Gaonach <mael.gaonach@esrf.fr>
2022-02-14 17:15:16 +0100

    fix ZoomImage alt

--- src/components/image/zoomimage.tsx
+++ src/components/image/zoomimage.tsx
@@ -1,17 +1,29 @@
 import Zoom from 'react-medium-image-zoom';
 import { Image } from 'react-bootstrap';
 import LazyLoad from 'react-lazyload';
+import { useState } from 'react';
+
+import './zoomimage.css';
 
 interface props {
   src: string;
   alt?: string;
 }
 export default function ZoomImage({ src, alt }: props) {
+  const [error, setError] = useState(false);
+
+  if (error) {
+    return (
+      <div className="zoom-image-alt">
+        <p>{alt} not found</p>
+      </div>
+    );
+  }
   return (
     <div style={{ margin: 5 }}>
-      <LazyLoad>
+      <LazyLoad height="100%" offset={300}>
         <Zoom>
-          <Image thumbnail src={src} alt={alt} />
+          <Image thumbnail src={src} alt={alt} onError={() => setError(true)} />
         </Zoom>
       </LazyLoad>
     </div>
