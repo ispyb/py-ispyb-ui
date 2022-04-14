ead5267c52b55758d77cb90a714d9f0564209a3f
Mael Gaonach <mael.gaonach@esrf.fr>
2022-04-14 15:23:20 +0200

    form validation

--- src/pages/shipping/addressmodal.tsx
+++ src/pages/shipping/addressmodal.tsx
@@ -1,7 +1,9 @@
 import LoadingPanel from 'components/loading/loadingpanel';
 import { ElementType, Suspense, useState } from 'react';
-import { Button, Col, Form, Modal, Row } from 'react-bootstrap';
+import { Button, Col, Form, InputGroup, Modal, Row } from 'react-bootstrap';
 import { LabContact } from './model';
+import { Formik, FormikProps } from 'formik';
+import * as Yup from 'yup';
 
 export default function AddressModal({ show, onHide, address }: { show: boolean; onHide: () => void; address: LabContact }) {
   return (
@@ -16,14 +18,6 @@ export default function AddressModal({ show, onHide, address }: { show: boolean;
           </Suspense>
         )}
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="primary" onClick={onHide}>
-          Save
-        </Button>
-        <Button variant="secondary" onClick={onHide}>
-          Cancel
-        </Button>
-      </Modal.Footer>
     </Modal>
   );
 }
@@ -52,6 +46,27 @@ type FormType = {
   labName: string;
   labAddress: string;
 };
+
+const schema = Yup.object().shape({
+  cardName: Yup.string().required('Mandatory field').max(40, 'Too long'),
+
+  givenName: Yup.string().max(45, 'Too long'),
+  familyName: Yup.string().max(100, 'Too long'),
+
+  emailAddress: Yup.string().email('Valid email required').max(60, 'Too long'),
+  phoneNumber: Yup.string().max(45, 'Too long'),
+  faxNumber: Yup.string().max(45, 'Too long'),
+
+  courierAccount: Yup.string().max(45, 'Too long'),
+  defaultCourrierCompany: Yup.string().max(45, 'Too long'),
+
+  dewarAvgCustomsValue: Yup.number().integer().min(0, 'Should be >=0').required('Mandatory field').typeError('A number is required'),
+  dewarAvgTransportValue: Yup.number().integer().min(0, 'Should be >=0').required('Mandatory field').typeError('A number is required'),
+  billingReference: Yup.string().max(45, 'Too long'),
+
+  labName: Yup.string().max(45, 'Too long'),
+  labAddress: Yup.string().max(45, 'Too long'),
+});
 
 export function AddressForm({ address }: { address: LabContact }) {
   const initialState: FormType = {
@@ -84,63 +99,65 @@ export function AddressForm({ address }: { address: LabContact }) {
   };
 
   return (
-    <Form noValidate>
-      <Row className="mb-3">
-        <FormInput name="Address name" field="cardName" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-      </Row>
-      <Row className="mb-3">
-        <FormInput name="First name" field="givenName" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-        <FormInput name="Surname" field="familyName" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-      </Row>
-      <Row className="mb-3">
-        <FormInput name="Email" field="emailAddress" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-        <FormInput name="Phone" field="phoneNumber" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-        <FormInput name="Fax" field="faxNumber" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-      </Row>
-      <Row className="mb-3">
-        <FormInput name="Courier account" field="courierAccount" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-        <FormInput name="Courier company" field="defaultCourrierCompany" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-      </Row>
-      <Row className="mb-3">
-        <FormInput name="Avg customs value" field="dewarAvgCustomsValue" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-        <FormInput name="Avg transport value" field="dewarAvgTransportValue" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-        <FormInput name="Billing reference" field="billingReference" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-      </Row>
-      <Row className="mb-3">
-        <FormInput name="Lab name" field="labName" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-      </Row>
-      <Row className="mb-3">
-        <FormInput name="Lab address" type={'textarea'} field="labAddress" formData={formData} updateFormProperty={updateFormProperty}></FormInput>
-      </Row>
-    </Form>
+    <Formik
+      initialValues={formData}
+      validationSchema={schema}
+      onSubmit={function (values, formikHelpers) {
+        return;
+      }}
+    >
+      {(formicProps) => (
+        <Form noValidate onSubmit={formicProps.handleSubmit}>
+          <Row className="mb-3">
+            <FormInput name="Address name" field="cardName" formicProps={formicProps}></FormInput>
+          </Row>
+          <Row className="mb-3">
+            <FormInput name="First name" field="givenName" formicProps={formicProps}></FormInput>
+            <FormInput name="Surname" field="familyName" formicProps={formicProps}></FormInput>
+          </Row>
+          <Row className="mb-3">
+            <FormInput name="Email" field="emailAddress" formicProps={formicProps}></FormInput>
+            <FormInput name="Phone" field="phoneNumber" formicProps={formicProps}></FormInput>
+            <FormInput name="Fax" field="faxNumber" formicProps={formicProps}></FormInput>
+          </Row>
+          <Row className="mb-3">
+            <FormInput name="Courier account" field="courierAccount" formicProps={formicProps}></FormInput>
+            <FormInput name="Courier company" field="defaultCourrierCompany" formicProps={formicProps}></FormInput>
+          </Row>
+          <Row className="mb-3">
+            <FormInput name="Avg customs value" field="dewarAvgCustomsValue" formicProps={formicProps}></FormInput>
+            <FormInput name="Avg transport value" field="dewarAvgTransportValue" formicProps={formicProps}></FormInput>
+            <FormInput name="Billing reference" field="billingReference" formicProps={formicProps}></FormInput>
+          </Row>
+          <Row className="mb-3">
+            <FormInput name="Lab name" field="labName" formicProps={formicProps}></FormInput>
+          </Row>
+          <Row className="mb-3">
+            <FormInput name="Lab address" type={'textarea'} field="labAddress" formicProps={formicProps}></FormInput>
+          </Row>
+          <Button type="submit">Save</Button>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
-export function FormInput({
-  name,
-  field,
-  formData,
-  updateFormProperty,
-  type,
-}: {
-  name: string;
-  field: keyof FormType;
-  formData: FormType;
-  // eslint-disable-next-line no-unused-vars
-  updateFormProperty: (field: keyof FormType, value: string) => void;
-  type?: ElementType<any>;
-}) {
+export function FormInput({ name, field, type, formicProps }: { name: string; field: keyof FormType; type?: ElementType; formicProps: FormikProps<FormType> }) {
   return (
     <Form.Group as={Col}>
       <Form.Label>{name}</Form.Label>
-      <Form.Control
-        as={type}
-        onChange={(event) => {
-          updateFormProperty(field, event.target.value);
-        }}
-        placeholder={'none'}
-        value={formData[field]}
-      />
+      <InputGroup hasValidation>
+        <Form.Control
+          type="text"
+          as={type}
+          placeholder={'none'}
+          onChange={formicProps.handleChange}
+          name={field}
+          value={formicProps.values[field]}
+          isInvalid={!!formicProps.errors[field]}
+        />
+        <Form.Control.Feedback type="invalid">{formicProps.errors[field]}</Form.Control.Feedback>
+      </InputGroup>
     </Form.Group>
   );
 }
