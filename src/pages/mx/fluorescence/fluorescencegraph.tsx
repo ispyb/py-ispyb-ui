dbf11b14464d052abe551758d2e795b37725de07
Maël Gaonach <mael.gaonach@esrf.fr>
2022-09-02 13:55:04 +0200

    refactor

--- src/pages/mx/fluorescence/fluorescencegraph.tsx
+++ src/pages/mx/fluorescence/fluorescencegraph.tsx
@@ -1,11 +1,9 @@
 import { parse } from 'papaparse';
 import { useXrfScanCsv } from 'hooks/ispyb';
 import { useState } from 'react';
-import { Button, ButtonGroup, Col, Row } from 'react-bootstrap';
-import { ResponsiveContainer, Tooltip, LineChart, ReferenceArea, CartesianGrid, XAxis, YAxis, Legend, Line, ReferenceLine } from 'recharts';
+import { Button } from 'react-bootstrap';
 import { FluorescenceSpectra } from '../model';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import InteractiveGraph from './interactivegraph';
 
 type Props = {
   proposalName: string;
@@ -25,13 +23,6 @@ export default function FluorescenceGraph({ proposalName, spectra }: Props) {
   const { data: csv, isError } = useXrfScanCsv({ proposalName, scanId: spectra.xfeFluorescenceSpectrumId });
 
   const [disabled, setDisabled] = useState<string[]>([]);
-  const [highlight, setHighlight] = useState<undefined | string>(undefined);
-
-  const [left, setLeft] = useState<undefined | number>(undefined);
-  const [right, setRight] = useState<undefined | number>(undefined);
-
-  const [refLeft, setRefLeft] = useState<undefined | number>(undefined);
-  const [refRight, setRefRight] = useState<undefined | number>(undefined);
 
   if (!csv || isError) {
     return <></>;
@@ -43,57 +34,32 @@ export default function FluorescenceGraph({ proposalName, spectra }: Props) {
 
   const maxValues: { [x: string]: { x: number; y: number; fit: number } } = {};
 
-  const r = parsed.data
-    .map((d) => {
-      const r: parseType = {};
-      for (const a in d) {
-        const x = Number(d['Energy']);
-        const y = Number(d[a]);
-        const fit = Number(d['fit']);
-        r[a] = y;
-        if (a.startsWith('y')) {
-          if (a in maxValues) {
-            const max = maxValues[a].y;
-            if (max < y) {
-              maxValues[a] = { x, y, fit };
-            }
-          } else {
+  const data = parsed.data.map((d) => {
+    const r: parseType = {};
+    for (const a in d) {
+      const x = Number(d['Energy']);
+      const y = Number(d[a]);
+      const fit = Number(d['fit']);
+      r[a] = y;
+      if (a.startsWith('y')) {
+        if (a in maxValues) {
+          const max = maxValues[a].y;
+          if (max < y) {
             maxValues[a] = { x, y, fit };
           }
+        } else {
+          maxValues[a] = { x, y, fit };
         }
       }
-      return r;
-    })
-    .filter((v: { [x: string]: number }) => {
-      if (left && right) {
-        return Number(left) <= v['Energy'] && Number(right) >= v['Energy'];
-      } else {
-        return true;
-      }
-    });
-
-  const doZoom = () => {
-    if (refLeft && refRight) {
-      setLeft(Math.min(refLeft, refRight));
-      setRight(Math.max(refLeft, refRight));
-      setRefLeft(undefined);
-      setRefRight(undefined);
     }
-  };
-  const unZoom = () => {
-    setLeft(undefined);
-    setRight(undefined);
-    setRefLeft(undefined);
-    setRefRight(undefined);
-  };
+    return r;
+  });
 
-  const hideAll = () => {
-    setDisabled(fields);
-  };
+  const tags: { [x: string]: number } = {};
 
-  const showAll = () => {
-    setDisabled([]);
-  };
+  for (const v in maxValues) {
+    tags[v] = maxValues[v].x;
+  }
 
   const showMostCommon = () => {
     const disabled = fields.filter((f) => {
@@ -111,164 +77,30 @@ export default function FluorescenceGraph({ proposalName, spectra }: Props) {
     setDisabled(disabled);
   };
 
-  return (
-    <Col>
-      <Row>
-        <div style={{ paddingRight: 20 }}>
-          <ResponsiveContainer width={'100%'} height={500}>
-            <LineChart
-              data={r}
-              onMouseDown={(e) => {
-                if (e) {
-                  setRefLeft(Number(e.activeLabel));
-                }
-              }}
-              onMouseMove={(e) => {
-                if (refLeft && e) {
-                  setRefRight(Number(e.activeLabel));
-                }
-              }}
-              onMouseUp={doZoom}
-              onMouseLeave={() => {
-                setRefLeft(undefined);
-                setRefRight(undefined);
-              }}
-            >
-              {refLeft && refRight ? <ReferenceArea x1={refLeft} x2={refRight} strokeOpacity={0.3} /> : null}
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis
-                dataKey="Energy"
-                type="number"
-                allowDataOverflow={Boolean(left && right)}
-                domain={[left || ((dataMin: number) => Math.trunc(dataMin)), right || ((dataMax: number) => Math.round(dataMax))]}
-              />
-              <YAxis type="number" domain={['auto', 'auto']} />
-              <Tooltip
-                active={true}
-                isAnimationActive={false}
-                content={({ active, payload, label }) => {
-                  if (!active) return null;
-                  return (
-                    <Col style={{ backgroundColor: 'white', border: '1px solid grey' }}>
-                      <Row>
-                        <Col></Col>
-                        <Col md={'auto'}>
-                          <h5>{label}</h5>
-                        </Col>
-                        <Col></Col>
-                      </Row>
-                      {payload &&
-                        payload
-                          .filter((p) => p.value != 0)
-                          .map((p) => {
-                            return (
-                              <Row>
-                                <p style={{ color: p.color, marginTop: 0, marginBottom: 0 }}>
-                                  {p.dataKey} = {p.value}
-                                </p>
-                              </Row>
-                            );
-                          })}
-                    </Col>
-                  );
-                }}
-              />
-              <Legend
-                onClick={(p) => {
-                  if (disabled.includes(p.dataKey)) {
-                    setDisabled(disabled.filter((e) => e !== p.dataKey));
-                  } else {
-                    const n = disabled.slice();
-                    n.push(p.dataKey);
-                    setDisabled(n);
-                  }
-                }}
-                onMouseEnter={(p) => {
-                  setHighlight(p.dataKey);
-                }}
-                onMouseLeave={() => {
-                  setHighlight(undefined);
-                }}
-              />
-              {fields.map((a, index) => (
-                <Line
-                  key={a}
-                  type="linear"
-                  legendType={disabled.includes(a) ? 'cross' : 'plainline'}
-                  dataKey={a}
-                  stroke={COLORS[index]}
-                  dot={false}
-                  strokeWidth={highlight === a ? 3 : 1}
-                  strokeDasharray={highlight && highlight !== a ? '5 5' : undefined}
-                  hide={disabled.includes(a) && highlight !== a}
-                  isAnimationActive={false}
-                ></Line>
-              ))}
-              {fields
-                .filter((a) => {
-                  if (disabled.includes(a) && highlight !== a) return false;
-                  if (!(a in maxValues)) return false;
-                  return true;
-                })
-                .sort((a, b) => {
-                  return maxValues[b].x - maxValues[a].x;
-                })
-                .map((a, index) => {
-                  const color = COLORS[fields.indexOf(a)];
-                  return (
-                    <ReferenceLine
-                      x={maxValues[a].x}
-                      stroke={color}
-                      strokeWidth={0.5}
-                      strokeDasharray={'10 10'}
-                      label={<ReferenceLabel value={a} fill={color} index={index} />}
-                    ></ReferenceLine>
-                  );
-                })}
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col md={'auto'}>
-          <p>
-            <FontAwesomeIcon style={{ marginRight: 5 }} icon={faQuestionCircle}></FontAwesomeIcon>Hover legend to highlight line, click to toggle visibility, select area on chart
-            to zoom.
-          </p>
-        </Col>
-        <Col></Col>
-      </Row>
-      <Row>
-        <Col></Col>
-        <Col md={'auto'}>
-          <ButtonGroup>
-            <Button onClick={unZoom}>reset zoom</Button>
-          </ButtonGroup>
-          <ButtonGroup style={{ marginLeft: 10 }}>
-            <Button onClick={hideAll}>hide all</Button>
-            <Button onClick={showAll}>show all</Button>
-          </ButtonGroup>
-          <ButtonGroup style={{ marginLeft: 10 }}>
-            <Button onClick={showMostCommon}>show common elements</Button>
-            <Button onClick={showAuto}>show fit</Button>
-          </ButtonGroup>
-        </Col>
-        <Col></Col>
-      </Row>
-    </Col>
-  );
-}
+  const onHideKeys = (keys: string[]) => {
+    const new_disabled = disabled.slice();
+    new_disabled.push(...keys);
+    setDisabled(new_disabled);
+  };
 
-export function ReferenceLabel({ fill, value, viewBox, index }: { fill: string; value: string; viewBox?: { x: number; y: number }; index: number }) {
-  const x = (viewBox?.x || 0) - 3 * value.length;
-  const y = (viewBox?.y || 0) + (index % 2) * 25;
+  const onShowKeys = (keys: string[]) => {
+    const new_disabled = disabled.filter((key) => !keys.includes(key));
+    setDisabled(new_disabled);
+  };
+
+  const extraButtons = [[<Button onClick={showMostCommon}>Show common elements</Button>, <Button onClick={showAuto}>Show auto fit</Button>]];
 
   return (
-    <foreignObject x={x} y={y} width={1} height={1} overflow={'visible'}>
-      <div style={{ backgroundColor: 'white', border: '1px solid grey', display: 'inline' }}>
-        <small style={{ color: fill, whiteSpace: 'nowrap' }}>{value}</small>
-      </div>
-    </foreignObject>
+    <InteractiveGraph
+      data={data}
+      colors={COLORS}
+      xKey={'Energy'}
+      tags={tags}
+      hiddenKeys={disabled}
+      keys={fields}
+      onHideKeys={onHideKeys}
+      onShowKeys={onShowKeys}
+      extraButtons={extraButtons}
+    ></InteractiveGraph>
   );
 }
---
+++ src/pages/mx/fluorescence/interactivegraph.tsx
@@ -0,0 +1,227 @@
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { Col, Row, ButtonGroup, Button } from 'react-bootstrap';
+import { ResponsiveContainer, Tooltip, LineChart, ReferenceArea, CartesianGrid, XAxis, YAxis, Legend, Line, ReferenceLine, TooltipProps } from 'recharts';
+
+type dataType = { [x: string]: number };
+
+type Props = {
+  data: dataType[];
+  keys: string[];
+  hiddenKeys: string[];
+  tags: { [x: string]: number };
+  // eslint-disable-next-line no-unused-vars
+  colors: string[];
+  xKey: string;
+  // eslint-disable-next-line no-unused-vars
+  onShowKeys: (keys: string[]) => void;
+  // eslint-disable-next-line no-unused-vars
+  onHideKeys: (keys: string[]) => void;
+  extraButtons: React.ReactElement[][];
+};
+
+export default function InteractiveGraph(props: Props) {
+  const [highlight, setHighlight] = useState<undefined | string>(undefined);
+
+  const [left, setLeft] = useState<undefined | number>(undefined);
+  const [right, setRight] = useState<undefined | number>(undefined);
+
+  const [refLeft, setRefLeft] = useState<undefined | number>(undefined);
+  const [refRight, setRefRight] = useState<undefined | number>(undefined);
+
+  const doZoom = () => {
+    if (refLeft && refRight) {
+      setLeft(Math.min(refLeft, refRight));
+      setRight(Math.max(refLeft, refRight));
+      setRefLeft(undefined);
+      setRefRight(undefined);
+    }
+  };
+  const unZoom = () => {
+    setLeft(undefined);
+    setRight(undefined);
+    setRefLeft(undefined);
+    setRefRight(undefined);
+  };
+
+  const hideAll = () => {
+    props.onHideKeys(props.keys);
+  };
+
+  const showAll = () => {
+    props.onShowKeys(props.keys);
+  };
+
+  const data_filtered = props.data.filter((v: { [x: string]: number }) => {
+    if (left && right) {
+      return Number(left) <= v[props.xKey] && Number(right) >= v[props.xKey];
+    } else {
+      return true;
+    }
+  });
+
+  return (
+    <Col>
+      <Row>
+        <div style={{ paddingRight: 20 }}>
+          <ResponsiveContainer width={'100%'} height={500}>
+            <LineChart
+              data={data_filtered}
+              onMouseDown={(e) => {
+                if (e) {
+                  setRefLeft(Number(e.activeLabel));
+                }
+              }}
+              onMouseMove={(e) => {
+                if (refLeft && e) {
+                  setRefRight(Number(e.activeLabel));
+                }
+              }}
+              onMouseUp={doZoom}
+              onMouseLeave={() => {
+                setRefLeft(undefined);
+                setRefRight(undefined);
+              }}
+            >
+              {refLeft && refRight ? <ReferenceArea x1={refLeft} x2={refRight} strokeOpacity={0.3} /> : null}
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey={props.xKey}
+                type="number"
+                allowDataOverflow={Boolean(left && right)}
+                domain={[left || ((dataMin: number) => Math.trunc(dataMin)), right || ((dataMax: number) => Math.round(dataMax))]}
+              />
+              <YAxis type="number" domain={['auto', 'auto']} />
+              <Tooltip active={true} isAnimationActive={false} content={RenderTooltip} />
+              <Legend
+                onClick={(p) => {
+                  if (props.hiddenKeys.includes(p.dataKey)) {
+                    props.onShowKeys([p.dataKey]);
+                  } else {
+                    props.onHideKeys([p.dataKey]);
+                  }
+                }}
+                onMouseEnter={(p) => {
+                  setHighlight(p.dataKey);
+                }}
+                onMouseLeave={() => {
+                  setHighlight(undefined);
+                }}
+              />
+              {renderLines({ ...props, highlight })}
+              {renderTags({ ...props, highlight })}
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col md={'auto'}>
+          <p>
+            <FontAwesomeIcon style={{ marginRight: 5 }} icon={faQuestionCircle}></FontAwesomeIcon>Hover legend to highlight line, click to toggle visibility, select area on chart
+            to zoom.
+          </p>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col md={'auto'}>
+          <ButtonGroup>
+            <Button onClick={unZoom}>reset zoom</Button>
+          </ButtonGroup>
+          <ButtonGroup style={{ marginLeft: 10 }}>
+            <Button onClick={hideAll}>hide all</Button>
+            <Button onClick={showAll}>show all</Button>
+          </ButtonGroup>
+          {props.extraButtons.map((group) => {
+            return <ButtonGroup style={{ marginLeft: 10 }}>{group}</ButtonGroup>;
+          })}
+        </Col>
+        <Col></Col>
+      </Row>
+    </Col>
+  );
+}
+
+export function renderLines(props: Props & { highlight?: string }) {
+  return props.keys.map((key) => (
+    <Line
+      key={key}
+      dataKey={key}
+      type="linear"
+      legendType={props.hiddenKeys.includes(key) ? 'cross' : 'plainline'}
+      stroke={props.colors[props.keys.indexOf(key)]}
+      dot={false}
+      strokeWidth={props.highlight === key ? 3 : 1}
+      strokeDasharray={props.highlight && props.highlight !== key ? '5 5' : undefined}
+      hide={props.hiddenKeys.includes(key) && props.highlight !== key}
+      isAnimationActive={false}
+    ></Line>
+  ));
+}
+
+export function renderTags(props: Props & { highlight?: string }) {
+  return props.keys
+    .filter((key) => {
+      if (props.hiddenKeys.includes(key) && props.highlight !== key) return false;
+      if (!(key in props.tags)) return false;
+      return true;
+    })
+    .sort((a, b) => {
+      return props.tags[b] - props.tags[a];
+    })
+    .map((key, index) => {
+      const color = props.colors[props.keys.indexOf(key)];
+      return (
+        <ReferenceLine
+          x={props.tags[key]}
+          stroke={color}
+          strokeWidth={0.5}
+          strokeDasharray={'10 10'}
+          label={<ReferenceLabel value={key} fill={color} index={index} />}
+        ></ReferenceLine>
+      );
+    });
+}
+
+export function RenderTooltip(props: TooltipProps<number, string>) {
+  if (!props.active) return null;
+  return (
+    <Col style={{ backgroundColor: 'white', border: '1px solid grey' }}>
+      <Row>
+        <Col></Col>
+        <Col md={'auto'}>
+          <h5>{props.label}</h5>
+        </Col>
+        <Col></Col>
+      </Row>
+      {props.payload &&
+        props.payload
+          .filter((p) => p.value != 0)
+          .map((p) => {
+            return (
+              <Row>
+                <p style={{ color: p.color, marginTop: 0, marginBottom: 0 }}>
+                  {p.dataKey} = {p.value}
+                </p>
+              </Row>
+            );
+          })}
+    </Col>
+  );
+}
+
+export function ReferenceLabel({ fill, value, viewBox, index }: { fill: string; value: string; viewBox?: { x: number; y: number }; index: number }) {
+  const x = (viewBox?.x || 0) - 3 * value.length;
+  const y = (viewBox?.y || 0) + (index % 2) * 25;
+
+  return (
+    <foreignObject x={x} y={y} width={1} height={1} overflow={'visible'}>
+      <div style={{ backgroundColor: 'white', border: '1px solid grey', display: 'inline' }}>
+        <small style={{ color: fill, whiteSpace: 'nowrap' }}>{value}</small>
+      </div>
+    </foreignObject>
+  );
+}
