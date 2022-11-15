b5d4932264779e897adfcd5d73e935a839b9af92
Maël Gaonach <mael.gaonach@esrf.fr>
2022-11-15 16:38:21 +0100

    fix color boundaries

--- src/helpers/ssx.ts
+++ src/helpers/ssx.ts
@@ -1,9 +1,9 @@
 export function getColorFromHitPercent(hitPercent: number) {
-  if (hitPercent >= 75) {
+  if (hitPercent >= 30) {
     return '#71db44';
-  } else if (hitPercent >= 50) {
+  } else if (hitPercent >= 10) {
     return '#a2cf4e';
-  } else if (hitPercent >= 25) {
+  } else if (hitPercent >= 2) {
     return '#edc132';
   }
   return '#c9483e';
