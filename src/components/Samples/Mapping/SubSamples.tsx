0b93904fbbde5f604dba67d5b034c57a65724e35
Stuart Fisher <stuart.fisher@esrf.fr>
2023-01-20 18:14:23 +0100

    disable events for subsamples

--- src/components/Samples/Mapping/SubSamples.tsx
+++ src/components/Samples/Mapping/SubSamples.tsx
@@ -88,6 +88,7 @@ export default function SubSamples({
                     : 'purple'
                 }
                 strokeWidth={strokeWidth}
+                listening={false}
               />
               <Text
                 key="text"
