731b52a2cd33445d317579bdf5720fa876dfb2c4
Maël Gaonach <mael.gaonach@esrf.fr>
2023-02-20 12:48:39 +0100

    improve molecule loading

--- src/components/Molecules/UglymolViewer.tsx
+++ src/components/Molecules/UglymolViewer.tsx
@@ -14,12 +14,18 @@ export type MolData = {
 export function UglyMolPreview({
   mol,
   title,
+  defaultShow = false,
 }: {
   mol: MolData;
   title: string;
+  defaultShow?: boolean;
 }) {
   const { ref, inView } = useInView({ rootMargin: '1000px 0px' });
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(defaultShow);
+
+  useEffect(() => {
+    if (!inView && show !== defaultShow) setShow(defaultShow);
+  }, [inView, show, defaultShow]);
 
   const fallback = (
     <LoadingUglyMolViewer
@@ -64,6 +70,7 @@ export function LoadingUglyMolViewer({
           height: height,
           width: width,
           display: 'flex',
+          flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
           backgroundColor: 'black',
@@ -74,9 +81,14 @@ export function LoadingUglyMolViewer({
             <span className="visually-hidden"></span>
           </Spinner>
         ) : (
-          <Button variant="outline-secondary" onClick={() => setShow(true)}>
-            Load molecule preview
-          </Button>
+          <>
+            <Button variant="outline-secondary" onClick={() => setShow(true)}>
+              Load molecule preview
+            </Button>
+            <small style={{ color: 'white' }}>
+              <i>Some molecules can be heavy to load.</i>
+            </small>
+          </>
         )}
       </div>
       <Row>
@@ -108,6 +120,10 @@ export function UglyMolViewer({
   const [idhelp] = useState(_.uniqueId('help-'));
   const [viewerObject, setViewerObject] = useState<Viewer | null>(null);
 
+  const [pdbLoaded, setPdbLoaded] = useState(false);
+  const [mapsLoaded, setMapsLoaded] = useState(false);
+  const [peaksLoaded, setPeaksLoaded] = useState(false);
+
   useEffect(() => {
     //create the viewer
     const opts = {
@@ -118,10 +134,10 @@ export function UglyMolViewer({
     };
     const viewer = new Viewer(opts);
     setViewerObject(viewer);
-    viewer.load_pdb(mol.pdb);
-    viewer.load_ccp4_maps(mol.map1, mol.map2);
-    if (mol.peaks) viewer.load_peaks(mol.peaks);
-
+    viewer.load_pdb(mol.pdb, null, () => setPdbLoaded(true));
+    viewer.load_ccp4_maps(mol.map1, mol.map2, () => setMapsLoaded(true));
+    if (mol.peaks) viewer.load_peaks(mol.peaks, () => setPeaksLoaded(true));
+    if (!mol.peaks) setPeaksLoaded(true);
     return () => {
       //To clear effect, remove any child added by uglymol
       const viewerElem = document.getElementById(idViewer);
@@ -142,67 +158,136 @@ export function UglyMolViewer({
         ?.getContext()
         ?.getExtension('WEBGL_lose_context')
         ?.loseContext();
+      setPdbLoaded(false);
+      setMapsLoaded(false);
+      setPeaksLoaded(false);
     };
   }, [mol, idViewer, idhud, idhelp]);
 
+  const getLoadingText = () => {
+    if (!pdbLoaded) return 'Loading PDB...';
+    if (!mapsLoaded) return 'Loading maps...';
+    if (!peaksLoaded) return 'Loading peaks...';
+    return 'Loading';
+  };
+
   return (
-    <Col>
-      <div
-        id={idViewer}
-        style={{ height: height, width: width, position: 'relative' }}
-      >
-        <span
-          id={idhud}
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            right: 0,
-            lineHeight: '1',
-            whiteSpace: 'pre-wrap',
-            backgroundColor: 'rgba(0,0,0,0.7)',
-            color: 'white',
-            pointerEvents: 'none',
-          }}
-        ></span>
-        <small
-          id={idhelp}
-          style={{
-            position: 'absolute',
-            left: 0,
-            bottom: 0,
-            top: 0,
-            overflow: 'auto',
-            lineHeight: '1',
-            whiteSpace: 'pre-wrap',
-            backgroundColor: 'black',
-            color: 'white',
-          }}
-          onMouseDown={(e) => {
-            e.preventDefault();
-          }}
-          onDoubleClick={(e) => {
-            e.preventDefault();
-          }}
-          onKeyDown={(e) => {
-            e.preventDefault();
-          }}
-        ></small>
-      </div>
-      <Row>
-        <Col></Col>
-        <Col xs={'auto'}>
-          <Button
-            variant="link"
-            size="sm"
-            onClick={() => {
-              viewerObject?.toggle_full_screen();
+    <>
+      <UglyMolLoadingOverlay
+        show={!pdbLoaded || !mapsLoaded || !peaksLoaded}
+        message={getLoadingText()}
+      />
+      <Col>
+        <div
+          id={idViewer}
+          style={{ height: height, width: width, position: 'relative' }}
+        >
+          <span
+            id={idhud}
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              right: 0,
+              lineHeight: '1',
+              whiteSpace: 'pre-wrap',
+              backgroundColor: 'rgba(0,0,0,0.7)',
+              color: 'white',
+              pointerEvents: 'none',
             }}
-          >
-            {title} - Fullscreen
-          </Button>
-        </Col>
-        <Col></Col>
-      </Row>
-    </Col>
+          ></span>
+          <small
+            id={idhelp}
+            style={{
+              position: 'absolute',
+              left: 0,
+              bottom: 0,
+              top: 0,
+              overflow: 'auto',
+              lineHeight: '1',
+              whiteSpace: 'pre-wrap',
+              backgroundColor: 'black',
+              color: 'white',
+            }}
+            onMouseDown={(e) => {
+              e.preventDefault();
+            }}
+            onDoubleClick={(e) => {
+              e.preventDefault();
+            }}
+            onKeyDown={(e) => {
+              e.preventDefault();
+            }}
+          ></small>
+        </div>
+        <Row>
+          <Col></Col>
+          <Col xs={'auto'}>
+            <Button
+              variant="link"
+              size="sm"
+              onClick={() => {
+                viewerObject?.toggle_full_screen();
+              }}
+            >
+              {title} - Fullscreen
+            </Button>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Col>
+    </>
+  );
+}
+
+function UglyMolLoadingOverlay({
+  show,
+  message,
+}: {
+  show: boolean;
+  message: string;
+}) {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        height: 50,
+        backgroundColor: 'black',
+        display: show ? 'flex' : 'none',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 10000,
+      }}
+    >
+      <Spinner
+        animation="border"
+        role="status"
+        style={{ color: 'white', height: 20, width: 20 }}
+      >
+        <span className="visually-hidden"></span>
+      </Spinner>
+      <strong
+        style={{
+          color: 'white',
+          fontSize: 20,
+          margin: 0,
+          marginLeft: 10,
+        }}
+      >
+        {message}
+      </strong>
+      <small
+        style={{
+          color: 'white',
+          margin: 0,
+          marginLeft: 10,
+        }}
+      >
+        <i>Browser might be slow.</i>
+      </small>
+    </div>
   );
 }
