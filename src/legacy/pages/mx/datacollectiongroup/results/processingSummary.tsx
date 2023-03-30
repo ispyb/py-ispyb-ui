de3576ae9498d584cc2473c3dd65ed1a617cf221
Maël Gaonach <mael.gaonach@esrf.fr>
2023-03-30 13:03:23 +0200

    improve pipeline summary

--- src/legacy/pages/mx/datacollectiongroup/results/processingSummary.tsx
+++ src/legacy/pages/mx/datacollectiongroup/results/processingSummary.tsx
@@ -6,10 +6,15 @@ import {
   faQuestionCircle,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { getSpaceGroup } from 'helpers/spacegroups';
 import { useAutoProcRanking, usePipelines } from 'hooks/mx';
-import { getRankedResults } from 'legacy/helpers/mx/results/resultparser';
+import {
+  AutoProcIntegration,
+  getRankedResults,
+} from 'legacy/helpers/mx/results/resultparser';
 import { useAutoProc } from 'legacy/hooks/ispyb';
 import { DataCollectionGroup } from 'legacy/pages/mx/model';
+import _ from 'lodash';
 import {
   Card,
   Col,
@@ -46,32 +51,85 @@ export default function ProcessingSummary({
     pipelinesSelection.pipelines
   );
 
+  const groupedResults = _(results)
+    .groupBy((r) => r.program)
+    .entries()
+    .sortBy((r) => r[0].toLowerCase())
+    .value();
+
   return (
     <Card.Footer>
       <Container fluid>
         <Row>
-          {results.map((r) => {
+          {groupedResults.map((r) => {
             return (
-              <Col sm={'auto'} key={r.id}>
-                <OverlayTrigger
-                  trigger={['focus', 'hover']}
-                  placement="bottom"
-                  overlay={
-                    <Popover>
-                      <Popover.Body>{r.status}</Popover.Body>
-                    </Popover>
-                  }
-                >
-                  <div>
-                    <StatusIcon status={r.status} /> <small>{r.program}</small>
-                  </div>
-                </OverlayTrigger>
-              </Col>
+              <PipelineStatus
+                key={r[0]}
+                pipeline={r[0]}
+                groupedResults={r[1]}
+              />
             );
           })}
         </Row>
       </Container>
     </Card.Footer>
+  );
+}
+
+function PipelineStatus({
+  pipeline,
+  groupedResults,
+}: {
+  pipeline: string;
+  groupedResults: AutoProcIntegration[];
+}) {
+  const statuses = groupedResults.map((r) => r.status);
+
+  const popover = (
+    <Popover>
+      <Popover.Header as="h3">{pipeline}</Popover.Header>
+      <Popover.Body>
+        <Col>
+          {groupedResults.map((r) => (
+            <Row key={r.programId}>
+              <Col xs={'auto'}>
+                <StatusIcon status={r.status} />
+              </Col>
+              <Col xs={'auto'}>
+                {r.spaceGroup
+                  ? getSpaceGroup(r.spaceGroup)?.name || r.spaceGroup
+                  : 'no space group'}
+              </Col>
+            </Row>
+          ))}
+        </Col>
+      </Popover.Body>
+    </Popover>
+  );
+
+  return (
+    <Col sm={'auto'}>
+      <OverlayTrigger
+        trigger={['focus', 'hover']}
+        placement="auto"
+        overlay={popover}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <div>
+            {statuses.map((s, i) => (
+              <StatusIcon key={i} status={s} />
+            ))}
+          </div>
+          <small>{pipeline}</small>
+        </div>
+      </OverlayTrigger>
+    </Col>
   );
 }
 
