f59a3833668dafb44cc0e524270429e61a16ab81
Maël Gaonach <mael.gaonach@esrf.fr>
2023-04-04 14:51:35 +0200

    paginate movies list for performance

--- src/legacy/pages/em/movie/moviespage.tsx
+++ src/legacy/pages/em/movie/moviespage.tsx
@@ -1,7 +1,15 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useMoviesByDataCollectionId } from 'legacy/hooks/ispyb';
-import { ButtonGroup, Badge, ToggleButton } from 'react-bootstrap';
+import {
+  ButtonGroup,
+  Badge,
+  ToggleButton,
+  Alert,
+  Button,
+  Col,
+  Row,
+} from 'react-bootstrap';
 import { MovieCard } from 'legacy/pages/em/movie/moviepanel';
 import Menu from 'legacy/components/menu/menu';
 import {
@@ -10,8 +18,14 @@ import {
   isResolutionLimitThreshold,
 } from 'legacy/pages/em/movie/helper';
 import { Movie } from 'legacy/pages/em/model';
+import Paginator from 'components/Layout/Paginator';
+import { usePaging } from 'hooks/usePaging';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBackward } from '@fortawesome/free-solid-svg-icons';
 
 export default function MoviesPage() {
+  const navigate = useNavigate();
+
   const { dataCollectionId = '', proposalName = '' } = useParams<{
     dataCollectionId: string;
     proposalName: string;
@@ -51,49 +65,94 @@ export default function MoviesPage() {
     );
   }
 
+  const pagination = usePaging(25, 0);
+
   return (
     <div style={{ marginBottom: '100px' }}>
       <Menu>
-        <ButtonGroup className="mb-2">
-          <ToggleButton
-            variant={isMotion ? 'outline-primary' : 'light'}
-            checked={isMotion}
-            id="tbg-btn-1"
-            value={1}
-            style={{ margin: 1 }}
-            size="sm"
-            type="checkbox"
-            onClick={() => setisMotion(!isMotion)}
-          >
-            Motion Threshold{' '}
-            <Badge bg="danger">{moviesReachingMoviewThreshold.length}</Badge>
-          </ToggleButton>
-          <ToggleButton
-            variant={isResolution ? 'outline-primary' : 'light'}
-            checked={isResolution}
-            id="tbg-btn-2"
-            value={2}
-            style={{ margin: 1 }}
-            size="sm"
-            type="checkbox"
-            onClick={() => setisResolution(!isResolution)}
-          >
-            Resolution Threshold{' '}
-            <Badge bg="danger">
-              {moviesReachingResolutionLimitThreshold.length}
-            </Badge>
-          </ToggleButton>
-        </ButtonGroup>
+        <Col>
+          <Row>
+            <Col xs={'auto'}>
+              <Button
+                variant={'secondary'}
+                onClick={() => {
+                  navigate('../summary');
+                }}
+              >
+                <FontAwesomeIcon
+                  icon={faBackward}
+                  style={{
+                    marginRight: '0.5rem',
+                  }}
+                />
+                Back to grid squares
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={'auto'} style={{ paddingTop: '1rem' }}>
+              <ButtonGroup className="mb-2">
+                <ToggleButton
+                  variant={isMotion ? 'outline-primary' : 'light'}
+                  checked={isMotion}
+                  id="tbg-btn-1"
+                  value={1}
+                  style={{ margin: 1 }}
+                  size="sm"
+                  type="checkbox"
+                  onClick={() => setisMotion(!isMotion)}
+                >
+                  Motion Threshold{' '}
+                  <Badge bg="danger">
+                    {moviesReachingMoviewThreshold.length}
+                  </Badge>
+                </ToggleButton>
+                <ToggleButton
+                  variant={isResolution ? 'outline-primary' : 'light'}
+                  checked={isResolution}
+                  id="tbg-btn-2"
+                  value={2}
+                  style={{ margin: 1 }}
+                  size="sm"
+                  type="checkbox"
+                  onClick={() => setisResolution(!isResolution)}
+                >
+                  Resolution Threshold{' '}
+                  <Badge bg="danger">
+                    {moviesReachingResolutionLimitThreshold.length}
+                  </Badge>
+                </ToggleButton>
+              </ButtonGroup>
+            </Col>
+          </Row>
+        </Col>
       </Menu>
-
-      {parsedMovies.map((movie: Movie) => (
-        <MovieCard
-          key={movie.Movie_movieId}
-          movie={movie}
-          proposalName={proposalName}
-          dataCollectionId={Number(dataCollectionId)}
-        />
-      ))}
+      {parsedMovies.length ? (
+        <>
+          <Paginator
+            total={parsedMovies.length}
+            skip={pagination.skip}
+            limit={pagination.limit}
+          />
+          {parsedMovies
+            .slice(pagination.skip, pagination.skip + pagination.limit)
+            .map((movie: Movie) => (
+              <MovieCard
+                key={movie.Movie_movieId}
+                movie={movie}
+                proposalName={proposalName}
+                dataCollectionId={Number(dataCollectionId)}
+              />
+            ))}
+          <Paginator
+            total={parsedMovies.length}
+            skip={pagination.skip}
+            limit={pagination.limit}
+          />
+        </>
+      ) : (
+        <Alert variant={'info'}>No movies found for selection</Alert>
+      )}
     </div>
   );
 }
